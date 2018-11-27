eaca074d91fbb54f046a425311d80c6e02ca84bc
L7907661 <hlbtaioe@163.com>
2018-11-26 19:24:33 -0500

    finish m6

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zeyu Liao.
 """
 ########################################################################
-# TODO: 1.
+# done: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# done: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,27 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+a = rg.SimpleTurtle('turtle')
+a.pen= rg.Pen('green', 3)
+a.speed = 30
+size = 300
+for k in range(12):
+    a.draw_circle(size)
+    a.pen_up()
+    a.forward(30)
+    a.pen_down()
+    size= size - 30
+#another
+window.tracer(50)
+b = rg.SimpleTurtle('turtle')
+b.pen =rg.Pen('pink', 5)
+for k in range(30):
+    b.pen_up()
+    b.left(20)
+    b.forward(20)
+    b.pen_down()
+    b.draw_regular_polygon(20,60)
+
+
