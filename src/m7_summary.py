2ecd79d24306eedac79cea91ba7bbb159da43b51
L7907661 <hlbtaioe@163.com>
2018-11-26 19:44:27 -0500

    For the first trail which the comment does not give the thickness and the error occurs if I do not type the thickness for the trail. So I pretend the thickness is 20.

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,10 +2,10 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zeyu Liao.
 """
 ########################################################################
-# TODO: 1.
+# done: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -43,3 +43,13 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+a = rg.SimpleTurtle()
+a.pen =rg.Pen('blue',20)
+a.left(90)
+a.forward(200)
+a.pen_up()
+a.go_to(rg.Point(100,-40))
+a.pen_down()
+a.pen = rg.Pen('green',10)
+a.backward(150)
